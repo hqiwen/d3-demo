53fbb8b58a3cb5251b3363c92b51ce83cc147be3
hqiwen <1726564970@qq.com>
2018-12-14 19:24:49 +0800

    add eraser for draw

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ let canvas = document.getElementById("canvas"),
     lineWidthSelect = document.getElementById("lineWidthSelect"),
     startAngleSelect = document.getElementById("startAngleSelect"),
     sidesSelect = document.getElementById("sidesSelect"),
+    eraserShapeSelect = document.getElementById("eraserShapeSelect"),
+    eraserWidthSelect = document.getElementById("eraserWidthSelect"),
+    drawRadio = document.getElementById("drawRadio"),
+    eraserRadio = document.getElementById("eraserRadio"),
     guideWireCheckBox = document.getElementById("guideWireCheckBox"),
     fillCheckBox = document.getElementById("fillCheckBox"),
     editCheckBox = document.getElementById("editCheckBox"),
@@ -18,6 +22,15 @@ let canvas = document.getElementById("canvas"),
     sides = 8,
     startAngle = 0,
     polygons = [],
+    ERASER_LINE_WIDTH = 1,
+    ERASER_SHADOW_COLOR = "rgb(0, 0, 0)",
+    ERASER_SHADOW_STYLE = "blue",
+    ERASER_STROKE_STYLE = 'rgb(0,0,225)',
+    ERASER_SHADOW_OFFSET = -5,
+    ERASER_SHADOW_BLUR = 20,
+    GRID_HORIZONTAL_SPACING = 10,
+    GRID_VERTICAL_SPACING = 10,
+    GRID_LINE_COLOR = 'lightblue',
     draggingOffsetX = null,
     draggingOffsetY = null;
 
@@ -83,8 +96,11 @@ Polygon.prototype = {
     }
 };
 function drawGrid(color, stepx, stepy) {
+    context.save()
     context.strokeStyle = color;
+    context.fillStyle = '#ffffff';
     context.lineWidth = 0.5;
+    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
     for (let i = stepx + 0.5; i < context.canvas.width; i += stepx) {
         context.beginPath();
         context.moveTo(i, 0);
@@ -97,9 +113,10 @@ function drawGrid(color, stepx, stepy) {
         context.lineTo(context.canvas.width, i);
         context.stroke();
     }
+    context.restore();
 }
 
-drawGrid('lightgray', 10, 10);
+drawGrid(GRID_LINE_COLOR, GRID_HORIZONTAL_SPACING, GRID_VERTICAL_SPACING);
 
 function windowToCanvas(x, y) {
     let bbox = canvas.getBoundingClientRect();
@@ -125,7 +142,7 @@ function updateRubberBandRectangle(loc) {
     else rubberBandRect.left = loc.y;
 }
 
-function drawRubberBandShape(loc, sides, startAngle) {
+function drawRubberBandPolygonShape(loc, sides, startAngle) {
     let polygon = new Polygon(mousedown.x, mousedown.y, rubberBandRect.width, parseInt(sides), (Math.PI / 180) * parseInt(startAngle), context.strokeStyle, context.fillStyle, fillCheckBox.checked);
     drawPolygon(polygon);
     if (!dragging) {
@@ -133,12 +150,31 @@ function drawRubberBandShape(loc, sides, startAngle) {
     }
 }
 
-function updateRubberBand(loc, sides, startAngle) {
-    updateRubberBandRectangle(loc);
-    drawRubberBandShape(loc, sides, startAngle);
+function drawRubberBandShape(loc) {
+    let angle = Math.atan(rubberBandRect.height / rubberBandRect.width), radius = rubberBandRect.height / Math.sin(angle);
+    if (mousedown.y === loc.y) {
+        radius = Math.abs(loc.x - mousedown.x);
+    }
+    context.beginPath();
+    context.arc(mousedown.x, mousedown.y, radius, 0, Math.PI *2, false);
+    context.stroke();
+    context.fill();
 }
 
-function drawPolygon(polygon) {
+function updateRubberBand(loc) {
+    updateRubberBandRectangle(loc);
+    drawRubberBandShape(loc);
+}
+
+function drawPolygon(polygon, angle) {
+    let tx = polygon.centerX,ty = polygon.centerY;
+    context.save();
+    context.translate(tx, ty);
+    if (angle) {
+        context.rotate(angle);
+    }
+    polygon.centerX = 0;
+    polygon.centerY = 0;
     context.beginPath();
     polygon.createPath(context);
     polygon.stroke(context);
@@ -146,6 +182,9 @@ function drawPolygon(polygon) {
     if (fillCheckBox.checked) {
         polygon.fill(context);
     }
+    context.restore();
+    polygon.centerX = tx;
+    polygon.centerY = ty;
 }
 function drawPolygons() {
     polygons.forEach(function (polygon) {
@@ -174,10 +213,72 @@ function drawGuideWires(x, y) {
     context.restore();
 }
 
+function setDrawPathForEraser(loc) {
+    let eraserWidth = parseFloat(eraserWidthSelect.value);
+
+    context.beginPath();
+
+    if (eraserShapeSelect.value === 'circle') {
+        context.arc(loc.x, loc.y,
+            eraserWidth / 2,
+            0, Math.PI * 2, false);
+    }
+    else {
+        context.rect(loc.x - eraserWidth / 2,
+            loc.y - eraserWidth / 2,
+            eraserWidth, eraserWidth);
+    }
+    context.clip();
+}
+
+function setErasePathForEraser() {
+    var eraserWidth = parseFloat(eraserWidthSelect.value);
+
+    context.beginPath();
+
+    if (eraserShapeSelect.value === 'circle') {
+        context.arc(lastX, lastY,
+            eraserWidth / 2 + ERASER_LINE_WIDTH,
+            0, Math.PI * 2, false);
+    }
+    else {
+        context.rect(lastX - eraserWidth / 2 - ERASER_LINE_WIDTH,
+            lastY - eraserWidth / 2 - ERASER_LINE_WIDTH,
+            eraserWidth + ERASER_LINE_WIDTH * 2,
+            eraserWidth + ERASER_LINE_WIDTH * 2);
+    }
+    context.clip();
+}
+function setEraserAttributes() {
+    context.lineWidth = ERASER_LINE_WIDTH;
+    context.shadowColor = ERASER_SHADOW_STYLE;
+    context.shadowOffsetX = ERASER_SHADOW_OFFSET;
+    context.shadowOffsetY = ERASER_SHADOW_OFFSET;
+    context.shadowBlur = ERASER_SHADOW_BLUR;
+    context.strokeStyle = ERASER_STROKE_STYLE;
+}
+function eraseLast() {
+    context.save();
+    setErasePathForEraser();
+    drawGrid(GRID_LINE_COLOR, GRID_HORIZONTAL_SPACING, GRID_VERTICAL_SPACING);
+    context.restore();
+}
+function drawEraser(loc) {
+    context.save();
+    setEraserAttributes();
+    setDrawPathForEraser(loc);
+    context.stroke();
+    context.restore();
+}
 function startDragging(loc) {
-    saveDrawingSurface();
+    if (drawRadio.checked) {
+        saveDrawingSurface();
+    }
     mousedown.x = loc.x;
     mousedown.y = loc.y;
+
+    lastX = loc.x;
+    lastY = loc.y;
 }
 function startEditing() {
     canvas.style.cursor = 'pointer';
@@ -204,10 +305,12 @@ canvas.onmousedown = function (e) {
     } else {
         startDragging(loc);
         dragging = true;
+        console.log("mousedown", drawRadio.checked);
     }
 }
 canvas.onmousemove = function (e) {
     let loc = windowToCanvas(e.clientX, e.clientY);
+    e.preventDefault();
     if (dragging && editing) {
         dragging.centerX = loc.x - draggingOffsetX;
         dragging.centerY = loc.y - draggingOffsetY;
@@ -216,23 +319,38 @@ canvas.onmousemove = function (e) {
         drawPolygons();
     } else {
         if (dragging) {
-            restoreDrawingSurface();
-            updateRubberBand(loc, sides, startAngle);
-            if (guideWires) {
-                drawGuideWires(mousedown.x, mousedown.y);
+            if (drawRadio.checked) {
+                restoreDrawingSurface();
+                updateRubberBand(loc);
+                if (guideWires) {
+                    drawGuideWires(mousedown.x, mousedown.y);
+                }
+            } else {
+                eraseLast();
+                drawEraser(loc);
             }
+            lastX = loc.x;
+            lastY = loc.y;
+            console.log("mousemove", lastX, lastY);
         }
     }
 }
 canvas.onmouseup = function (e) {
     let loc = windowToCanvas(e.clientX, e.clientY);
-    dragging = false;
+ 
     if (editing) {
 
-     } else {
-        restoreDrawingSurface();
-        updateRubberBand(loc, sides, startAngle);
+    } else {
+        if (drawRadio.checked) {
+            restoreDrawingSurface();
+            updateRubberBand(loc);
+        }
+        if (eraserRadio.checked) {
+            eraseLast();
+        }
     }
+    dragging = false;
+    console.log("mouseup");
 }
 eraseAllButton.onclick = function (e) {
     context.clearRect(0, 0, canvas.width, canvas.height);
